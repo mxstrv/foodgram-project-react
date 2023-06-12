60fab69a1826fa8053ce8ef544c7ea0e3f686d86
mxstrv <37843972+mxstrv@users.noreply.github.com>
2023-06-12 11:27:23 +0300

    modify urls.py and add custom user viewset

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,19 +1,20 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from djoser.views import TokenCreateView, TokenDestroyView
 
-# from .views import ...
+from .views import CustomUserViewSet
 
 
 v1_router = DefaultRouter()
-v1_router.register('users', UserViewSet, basename='users')
+v1_router.register('users', CustomUserViewSet, basename='users')
 
 
 auth_patterns = [
-    path('login/', TokenReceive.as_view(), name='user_login'),
-    path('logout/', TokenDelete.as_view(), name='user_logout'),
-]
+     path('login/', TokenCreateView.as_view(), name='user_login'),
+     path('logout/', TokenDestroyView.as_view(), name='user_logout'),
+ ]
 
 urlpatterns = [
     path('', include(v1_router.urls)),
-    path('/auth/token/', include(auth_patterns)),
+    path('auth/token/', include(auth_patterns)),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,19 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework.pagination import PageNumberPagination
+from djoser.views import UserViewSet
 
 from users.models import CustomUser
 from .permissions import IsAuthenticatedOrReadOnlyForProfile
 from .serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class CustomUserViewSet(UserViewSet):
     queryset = CustomUser.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnlyForProfile, ]
     serializer_class = UserSerializer
+    pagination_class = PageNumberPagination
 
+    def get_object(self):
+        # Для GET запроса по id пользователя
+        return get_object_or_404(CustomUser,
+                                 id=self.kwargs.get('id'))
