fe973e4d9cec9782145399d985809c7bb5caf00e
mxstrv <37843972+mxstrv@users.noreply.github.com>
2023-06-14 21:15:02 +0300

    pep fixes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import get_object_or_404
+from djoser.views import UserViewSet
 from rest_framework import viewsets, status
-from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import (IsAuthenticatedOrReadOnly,
                                         IsAuthenticated)
-from djoser.views import UserViewSet
 
 from recipes.models import Tag, Recipe, Ingredient
 from users.models import CustomUser, Subscription
