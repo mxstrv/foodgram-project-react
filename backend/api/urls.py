f475e4c4f176f0affe70814e32fef3b265256dcf
mxstrv <37843972+mxstrv@users.noreply.github.com>
2023-06-20 22:03:34 +0300

    production ready

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from djoser.views import TokenCreateView, TokenDestroyView
-from django.conf import settings
-from django.conf.urls.static import static
 
 from .views import (CustomUserViewSet, TagViewSet,
                     RecipeViewSet, IngredientViewSet)
@@ -23,9 +21,3 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/token/', include(auth_patterns)),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT)
-# TODO DELETE BEFORE PROD
